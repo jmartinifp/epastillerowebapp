c5dffac9f5bfcb6c15b1c1df5aa9825f4839d0a4
jmartinifp <153239640+jmartinifp@users.noreply.github.com>
2026-04-16 20:45:06 +0200

    Update Config.js

--- assets/config/Config.js
+++ assets/config/Config.js
@@ -3,6 +3,8 @@ export class Config
     constructor()
     {
          this.urlPastillas="http://localhost:9876/pastillas/1";
+        this.urlPastillas="https://epastillerowebapp.vercel.app/api_estatica/pastillas.json";
+        
          this.urlUsuario="http://localhost:9876/usuario/1";
 
     }
